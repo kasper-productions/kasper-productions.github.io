7d86cb05b425c4084a70253fe7e5f3402debd20f
kavilla <kavilla@wisc.edu>
2017-05-06 20:57:10 -0500

    idk what i did

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,120 +7,183 @@
  *
  * Main module of the application.
  */
-angular
-  .module('kApp', [
-    'oc.lazyLoad',
-    'ui.router',
-    'ui.bootstrap',
-    'angular-loading-bar'
-  ])
-  .config(['$stateProvider','$urlRouterProvider','$ocLazyLoadProvider',function ($stateProvider,$urlRouterProvider,$ocLazyLoadProvider) {
-    
-    $ocLazyLoadProvider.config({
-      debug:false,
-      events:true
-    });
+angular.module('kApp', [
 
-    $urlRouterProvider.otherwise('/dashboard/home');
+        // Libraries
+        'oc.lazyLoad',
+        'ui.router',
+        'ui.bootstrap',
+        'angular-loading-bar'
+    ]
+).config(['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider',
+    function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
 
-    $stateProvider
-      .state('dashboard', {
-        url:'/dashboard',
-        templateUrl: 'views/dashboard/main.html',
-        resolve: {
-            loadMyDirectives:function($ocLazyLoad){
-                return $ocLazyLoad.load(
-                {
-                    name:'kApp',
-                    files:[
-                    'scripts/directives/header/header.js',
-                    'scripts/directives/header/header-notification/header-notification.js',
-                    'scripts/directives/sidebar/sidebar.js',
-                    'scripts/directives/sidebar/sidebar-search/sidebar-search.js'
-                    ]
-                })
-            }
-        }
-    })
-      .state('dashboard.home',{
-        url:'/home',
-        controller: 'MainCtrl',
-        templateUrl:'views/dashboard/home.html',
-        resolve: {
-          loadMyFiles:function($ocLazyLoad) {
-            return $ocLazyLoad.load({
-              name:'kApp',
-              files:[
-              'scripts/controllers/main.js',
-              'scripts/directives/timeline/timeline.js',
-              'scripts/directives/notifications/notifications.js',
-              'scripts/directives/chat/chat.js',
-              'scripts/directives/dashboard/stats/stats.js'
-              ]
+        $ocLazyLoadProvider.config({
+            debug: false,
+            events: true
+        });
+
+        $urlRouterProvider.otherwise('/dashboard/home');
+
+        $stateProvider
+            .state('dashboard', {
+                url: '/dashboard',
+                templateUrl: 'views/dashboard/main.html',
+                resolve: {
+                    loadMyDirectives: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load(
+                            {
+                                name: 'kApp',
+                                files: [
+                                    'scripts/directives/header/header.js',
+                                    'scripts/directives/header/header-notification/header-notification.js',
+                                    'scripts/directives/sidebar/sidebar.js',
+                                    'scripts/directives/sidebar/sidebar-search/sidebar-search.js'
+                                ]
+                            })
+                    }
+                }
             })
-          }
-        }
-      })
-      .state('dashboard.form',{
-        templateUrl:'views/form.html',
-        url:'/form'
-    })
-      .state('dashboard.blank',{
-        templateUrl:'views/pages/blank.html',
-        url:'/blank'
-    })
-      .state('login',{
-        templateUrl:'views/pages/login.html',
-        url:'/login'
-    })
-      .state('dashboard.chart',{
-        templateUrl:'views/chart.html',
-        url:'/chart',
-        controller:'ChartCtrl',
-        resolve: {
-          loadMyFile:function($ocLazyLoad) {
-            return $ocLazyLoad.load({
-              name:'chart.js',
-              files:[
-                'bower_components/angular-chart.js/dist/angular-chart.min.js',
-                'bower_components/angular-chart.js/dist/angular-chart.css'
-              ]
-            }),
-            $ocLazyLoad.load({
-                name:'kApp',
-                files:['scripts/controllers/chartContoller.js']
+            .state('dashboard.home', {
+                url: '/home',
+                controller: 'MainCtrl',
+                templateUrl: 'states/home/home.html',
+                resolve: {
+                    loadMyFiles: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load({
+                            name: 'kApp',
+                            files: [
+                                'scripts/controllers/main.js',
+                                'scripts/directives/timeline/timeline.js',
+                                'scripts/directives/notifications/notifications.js',
+                                'scripts/directives/chat/chat.js',
+                                'scripts/directives/dashboard/stats/stats.js'
+                            ]
+                        })
+                    }
+                }
             })
-          }
-        }
-    })
-      .state('dashboard.table',{
-        templateUrl:'views/table.html',
-        url:'/table'
-    })
-      .state('dashboard.panels-wells',{
-          templateUrl:'views/ui-elements/panels-wells.html',
-          url:'/panels-wells'
-      })
-      .state('dashboard.buttons',{
-        templateUrl:'views/ui-elements/buttons.html',
-        url:'/buttons'
-    })
-      .state('dashboard.notifications',{
-        templateUrl:'views/ui-elements/notifications.html',
-        url:'/notifications'
-    })
-      .state('dashboard.typography',{
-       templateUrl:'views/ui-elements/typography.html',
-       url:'/typography'
-   })
-      .state('dashboard.icons',{
-       templateUrl:'views/ui-elements/icons.html',
-       url:'/icons'
-   })
-      .state('dashboard.grid',{
-       templateUrl:'views/ui-elements/grid.html',
-       url:'/grid'
-   })
-  }]);
+
+            .state('dashboard.about', {
+                url: '/about',
+                controller: 'AboutCtrl',
+                templateUrl: 'states/about/about.html',
+                resolve: {
+                    loadMyFiles: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load({
+                            name: 'kApp',
+                            files: [
+                                'states/about/aboutController.js'
+                            ]
+                        })
+                    }
+                }
+            })
+
+            .state('dashboard.contact', {
+                url: '/contact',
+                controller: 'ContactCtrl',
+                templateUrl: 'states/contact/contact.html',
+                resolve: {
+                    loadMyFiles: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load({
+                            name: 'kApp',
+                            files: [
+                                'states/contact/contactController.js'
+                            ]
+                        })
+                    }
+                }
+            })
+
+            .state('dashboard.photos', {
+                url: '/photos',
+                controller: 'PhotosCtrl',
+                templateUrl: 'states/photos/photos.html',
+                resolve: {
+                    loadMyFiles: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load({
+                            name: 'kApp',
+                            files: [
+                                'states/photos/photosController.js'
+                            ]
+                        })
+                    }
+                }
+            })
+
+            .state('dashboard.requests', {
+                url: '/requests',
+                controller: 'RequestsCtrl',
+                templateUrl: 'states/requests/requests.html',
+                resolve: {
+                    loadMyFiles: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load({
+                            name: 'kApp',
+                            files: [
+                                'states/requests/requestsController.js'
+                            ]
+                        })
+                    }
+                }
+            })
+
+            .state('dashboard.videos', {
+                url: '/videos',
+                controller: 'VideosCtrl',
+                templateUrl: 'states/videos/videos.html',
+                resolve: {
+                    loadMyFiles: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load({
+                            name: 'kApp',
+                            files: [
+                                'states/videos/videosController.js'
+                            ]
+                        })
+                    }
+                }
+            })
+
+            .state('dashboard.form', {
+                templateUrl: 'views/form.html',
+                url: '/form'
+            })
+            .state('dashboard.blank', {
+                templateUrl: 'views/pages/blank.html',
+                url: '/blank'
+            })
+            .state('login', {
+                templateUrl: 'views/pages/login.html',
+                url: '/login'
+            })
+            .state('dashboard.table', {
+                templateUrl: 'views/table.html',
+                url: '/table'
+            })
+            .state('dashboard.panels-wells', {
+                templateUrl: 'views/ui-elements/panels-wells.html',
+                url: '/panels-wells'
+            })
+            .state('dashboard.buttons', {
+                templateUrl: 'views/ui-elements/buttons.html',
+                url: '/buttons'
+            })
+            .state('dashboard.notifications', {
+                templateUrl: 'views/ui-elements/notifications.html',
+                url: '/notifications'
+            })
+            .state('dashboard.typography', {
+                templateUrl: 'views/ui-elements/typography.html',
+                url: '/typography'
+            })
+            .state('dashboard.icons', {
+                templateUrl: 'views/ui-elements/icons.html',
+                url: '/icons'
+            })
+            .state('dashboard.grid', {
+                templateUrl: 'views/ui-elements/grid.html',
+                url: '/grid'
+            })
+    }]);
 
     
