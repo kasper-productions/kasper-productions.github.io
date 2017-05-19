5dbcf38dd9f11c38e8c03551c5cc6671dd94a4b8
kavilla <kavilla@wisc.edu>
2017-05-19 15:38:15 -0500

    added a lot of nice styles

--- app/states/photos/photosController.js
+++ app/states/photos/photosController.js
@@ -3,17 +3,44 @@
  */
 angular.module('kApp').controller('PhotosCtrl',
     function ($scope) {
-        $scope.images = [
-            { id: 1, thumbUrl: '../../assets/kasper-drive-thumbs/DSC00578.JPG', url: '../../assets/kasper-drive/DSC00578.JPG'},
-            { id: 2, thumbUrl: '../../assets/kasper-drive-thumbs/DSC00579.JPG', url: '../../assets/kasper-drive/DSC00579.JPG'},
-            { id: 3, thumbUrl: '../../assets/kasper-drive-thumbs/DSC00580.JPG', url: '../../assets/kasper-drive/DSC00580.JPG'},
-            { id: 4, thumbUrl: '../../assets/kasper-drive-thumbs/DSC00581.JPG', url: '../../assets/kasper-drive/DSC00581.JPG'},
-            { id: 5, thumbUrl: '../../assets/kasper-drive-thumbs/DSC00582.JPG', url: '../../assets/kasper-drive/DSC00582.JPG'},
-            { id: 6, thumbUrl: '../../assets/kasper-drive-thumbs/DSC00584.JPG', url: '../../assets/kasper-drive/DSC00584.JPG'},
-            { id: 7, thumbUrl: '../../assets/kasper-drive-thumbs/DSC00586.JPG', url: '../../assets/kasper-drive/DSC00586.JPG'},
-            { id: 8, thumbUrl: '../../assets/kasper-drive-thumbs/DSC00587.JPG', url: '../../assets/kasper-drive/DSC00587.JPG'},
-            { id: 9, thumbUrl: '../../assets/kasper-drive-thumbs/DSC00826.JPG', url: '../../assets/kasper-drive/DSC00826.JPG'},
-            { id: 10, thumbUrl: '../../assets/kasper-drive-thumbs/DSC00840.JPG', url: '../../assets/kasper-drive/DSC00840.JPG'}
+
+        $scope.fileNames = [
+            'Andrew 1.jpg', 'Chloe.jpg', 'David.jpg',
+            'DSC00578.JPG', 'DSC00579.JPG', 'DSC00580.JPG',
+            'DSC00581.JPG', 'DSC00582.JPG', 'DSC00584.JPG',
+            'DSC00586.JPG', 'DSC00587.JPG', 'DSC00771.JPG',
+            'DSC00772.JPG', 'DSC00774.JPG', 'DSC00775.JPG',
+            'DSC00776.JPG', 'DSC00777.JPG', 'DSC00778.JPG',
+            'DSC00779.JPG', 'DSC00780.JPG', 'DSC00781.JPG',
+            'DSC00796.JPG', 'DSC00797.JPG', 'DSC00802.JPG',
+            'DSC00803.JPG', 'DSC00805.JPG', 'DSC00806.JPG',
+            'DSC00807.JPG', 'DSC00809.JPG', 'DSC00816.JPG',
+            'DSC00817.JPG', 'DSC00822.JPG', 'DSC00826.JPG',
+            'DSC00827.JPG', 'DSC00829.JPG', 'DSC00830.JPG',
+            'DSC00831.JPG', 'DSC00840.JPG', 'DSC00859.JPG',
+            'Elena.jpg', 'Eva.jpg', 'DSC00840.JPG', 'DSC00859.JPG',
+            'IMG_9525.JPG', 'IMG_9527.JPG', 'IMG_9663.JPG',
+            'Ivory1.jpg', 'Jeff 1.jpg', 'Jeff.jpg',
+            'Jihan.jpg', 'Jin-An2.jpg', 'Jing Jing.jpg',
+            'Karie 1.jpg', 'Kathy 2.jpg', 'Kayla 1.jpg',
+            'Lisa 1.jpg', 'Ruilin.jpg', 'Yodi 1.jpg',
+            'Yodi 2.jpg', 'Yodi.jpg'
+
         ];
+
+        $scope.$on('$viewContentLoaded', function(){
+            $scope.images = [];
+
+            for (var i = 0; i < $scope.fileNames.length; i += 1) {
+                var fileName = $scope.fileNames[i];
+                var imgObj = {
+                    id: i + 1,
+                    thumbUrl: '../../assets/kasper-drive-thumbs/' + fileName,
+                    url: '../../assets/kasper-drive/' + fileName
+                };
+
+                $scope.images.push(imgObj);
+            }
+        });
     }
 );
