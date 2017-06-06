94cb706994f9e43216a5780c6cabdfb0cf0f5c38
kavilla <kavilla@wisc.edu>
2017-06-06 01:07:52 -0500

    made it functional already baby

--- app/states/videos/videosController.js
+++ app/states/videos/videosController.js
@@ -3,13 +3,21 @@
  */
 angular.module('kApp').controller('VideosCtrl',
     function ($scope, $sce) {
-        $scope.currentVideo = "https://www.youtube.com/embed/eu5c5ETMMKs";
-
+        //First id is default video
         var videoIds = [
             'eu5c5ETMMKs', '4ZwfnyrWWd0', 'SbPWDanATyQ',
             '1tCM-TADM6k', 'lcKH-ngMCM8', 'xHU-eFVxE3k',
             '7HpvwF_PpPk', 'R544oZHHF-U', 'iKUlsk2R_YU'
         ];
+
+        var defaultVideoObject = {
+            id: videoIds[0],
+            thumbnail: "https://img.youtube.com/vi/" + videoIds[0] + "/hqdefault.jpg",
+            embed: "https://www.youtube.com/embed/" + videoIds[0]
+        };
+
+        $scope.currentVideo = defaultVideoObject;
+
 
         $scope.videoObjects1 = [];
         $scope.videoObjects2 = [];
@@ -49,7 +57,11 @@ angular.module('kApp').controller('VideosCtrl',
         });
 
         $scope.getCurrentVideo = function () {
-            return $sce.trustAsResourceUrl($scope.currentVideo);
+            return $sce.trustAsResourceUrl($scope.currentVideo.embed);
+        };
+
+        $scope.selectVideo = function (videoObject) {
+            $scope.currentVideo = videoObject;
         }
     }
 );
