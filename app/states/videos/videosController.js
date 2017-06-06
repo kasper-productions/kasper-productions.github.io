e7e7a9bb5937de7836ec4cae8a6ca6edcedb6578
kavilla <kavilla@wisc.edu>
2017-06-05 20:15:47 -0500

    started putting in the work for videos page. the main video needs to be styled. and i need to put the video list in array of three i think with images as preview and on highlight shows video name

--- app/states/videos/videosController.js
+++ app/states/videos/videosController.js
@@ -2,7 +2,15 @@
  * Created by kawika on 5/4/17.
  */
 angular.module('kApp').controller('VideosCtrl',
-    function ($scope) {
+    function ($scope, $sce) {
+        $scope.currentVideo = "https://www.youtube.com/embed/eu5c5ETMMKs";
 
+        $scope.$on('$viewContentLoaded', function() {
+
+        });
+
+        $scope.getCurrentVideo = function () {
+            return $sce.trustAsResourceUrl($scope.currentVideo);
+        }
     }
 );
